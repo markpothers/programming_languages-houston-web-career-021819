4b36e12517fa18ec3009c40838c6db10db6db9e1
Mark Pothecary <markpothers@hotmail.com>
2019-02-14 21:29:30 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -19,53 +19,5 @@ def reformat_languages(languages)
       new_hash[name][:style] << :functional
     end
   end
-binding.pry
   new_hash
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
-
-  }
-}
-
-reformat_languages(languages)
